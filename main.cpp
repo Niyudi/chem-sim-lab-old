2ebba03bdc8a0600e78fede2b40288905a41afaa
Pedro Yudi Honda <niyudi.honda@gmail.com>
2021-08-26 15:26:55 -0300

    First run

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: niyudi
+ *
+ * Created on 26 de agosto de 2021, 15:25
+ */
+
+#include <QApplication>
+#include <QMainWindow>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+    
+    QMainWindow window;
+    window.show();
+
+    // create and show your widgets here
+
+    return app.exec();
+}
