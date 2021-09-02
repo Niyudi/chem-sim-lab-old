4545d82c2ddd76c5c0bc40964be9bb8711e8460a
Pedro Yudi Honda <niyudi.honda@gmail.com>
2021-09-02 16:14:16 -0300

    Added home page!

--- src/interface.cpp
+++ src/interface.cpp
@@ -5,6 +5,8 @@
 
 #include <QApplication>
 #include <QAction>
+#include <QHBoxLayout>
+#include <QLabel>
 #include <QMenu>
 #include <QMenuBar>
 #include <QWidget>
@@ -21,14 +23,56 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
     this->setWindowTitle("ChemSimLab");
     
     // Menu
+    auto* open_home_page = new QAction("Home", this);
+    auto* open_gas_simulator = new QAction("Gas simulator", this);
     auto* quit = new QAction("&Quit", this);
 
     QMenu* file = menuBar()->addMenu("&File");
+    
+    file->addAction(open_home_page);
+    file->addAction(open_gas_simulator);
+    
+    file->addSeparator();
     file->addAction(quit);
 
+    connect(open_home_page, &QAction::triggered, this, &MainWindow::openHome);
+    connect(open_gas_simulator, &QAction::triggered, this, &MainWindow::openGasSimulator);
     connect(quit, &QAction::triggered, qApp, QApplication::quit);
     
     // Central widget
-    auto* gas_simulator_widget = new GasSimulatorWidget(this);
-    setCentralWidget(gas_simulator_widget);
+    this->setCentralWidget(new HomePage(this));
+}
+
+void MainWindow::openHome() {
+    if (this->page == HOME_PAGE) {
+        return;
+    } else {
+        this->page = HOME_PAGE;
+        delete this->centralWidget();
+        this->setCentralWidget(new HomePage(this));
+    }
+}
+
+void MainWindow::openGasSimulator() {
+    if (this->page == GAS_SIMULATOR_PAGE) {
+        return;
+    } else {
+        this->page = GAS_SIMULATOR_PAGE;
+        delete this->centralWidget();
+        this->setCentralWidget(new GasSimulatorWidget(this));
+    }
+}
+
+// HomePage
+
+HomePage::HomePage(QWidget* parent) : QWidget(parent) {
+    auto* level0_hbox0 = new QHBoxLayout(this);
+    level0_hbox0->setContentsMargins(0, 0, 0, 0);
+    level0_hbox0->setSpacing(0);
+    
+    auto* label = new QLabel("Welcome!", this);
+    
+    level0_hbox0->addStretch(1);
+    level0_hbox0->addWidget(label, 0);
+    level0_hbox0->addStretch(1);
 }
--- src/interface.h
+++ src/interface.h
@@ -3,9 +3,30 @@
 
 #include <QMainWindow>
 
+// Enums
+
+enum Pages {
+    HOME_PAGE,
+    GAS_SIMULATOR_PAGE
+};
+
+// Classes
+
 class MainWindow : public QMainWindow {
+    Q_OBJECT
 public:
     MainWindow(QWidget* parent = nullptr);
+public slots:
+    void openHome();
+    void openGasSimulator();
+private:
+    Pages page = HOME_PAGE;
+};
+
+class HomePage : public QWidget {
+    Q_OBJECT
+public:
+    HomePage(QWidget* parent = nullptr);;
 };
 
 
